fb1fc9ac38cb1be1f9975626202e8484c283368b
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-04-01 23:35:59 +0900

    init

---
+++ src/main/java/project/customFI/customFIApplication.java
@@ -0,0 +1,13 @@
+package project.customFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class customFIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(customFIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/customFI/customFIApplicationTests.java
@@ -0,0 +1,13 @@
+package project.customFI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class customFIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
